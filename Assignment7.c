488e3ecc96339ba8329c8984daf7e9b0c51c1ab6
Ridhima-s <128236926+Ridhima-s@users.noreply.github.com>
2023-04-16 22:16:03 +0530

    Update Assignment7.c

--- Assignment7.c
+++ Assignment7.c
@@ -2,5 +2,18 @@
 
 int main()
 {
-  return 0;
+  char name[50];
+	int i,count=0;
+	printf("enter a string: ");
+	scanf("%s",&name);
+	for(i=0;name[i]!='\0';i++)
+	{
+		count++;
+	}
+	printf(" lenght of the string is %d ",count);
+	printf("\nthe reverse of name is : ");
+	for(i=count;i>=0;i--)
+	{
+		printf("%c",name[i]);
+	}
 }

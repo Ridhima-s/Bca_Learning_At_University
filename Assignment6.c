96b5b36abe88b689d8b3609e43a34495e455a264
Ridhima-s <128236926+Ridhima-s@users.noreply.github.com>
2023-04-16 22:14:18 +0530

    Update Assignment6.c

--- Assignment6.c
+++ Assignment6.c
@@ -2,5 +2,14 @@
 
 int main()
 {
+  int arr[]={1,1,2,3,4};
+	int n,i,count=0;
+	n=sizeof(arr)/sizeof(arr[0]);
+	for(i=0;i<n-1;i++)
+	{
+		if(arr[i]!=arr[i+1])
+		count++;
+	}
+	printf("Number of distinct numbers in this array is %d ",count+1);
   return 0;
 }

0cc128679064c69e4ecf3eca86c6c2adccff8a59
Ridhima-s <128236926+Ridhima-s@users.noreply.github.com>
2023-04-16 22:03:16 +0530

    Update Assignment1.c

--- Assignment1.c
+++ Assignment1.c
@@ -2,6 +2,23 @@
 
 int main()
 {
-  //add your code here
+  int i,a,tempeven=0,tempodd=0,n;
+	printf("Enter the total numbers you want to enter: ");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+	    printf("Enter number %d : ",i+1);
+	    scanf("%d",&a);
+	    if(a%2==0)
+	    {
+	    	tempeven++;
+		}
+		else
+		{
+		tempodd++;
+	    }
+    }
+    printf("\nTotal even numbers are %d",tempeven);
+    printf("\nTotal odd numbers are %d",tempodd);
   return 0;
 }

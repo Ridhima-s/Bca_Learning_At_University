6f38dfd31eb5d81bf144d9e2f9c34e4970599da4
Ridhima-s <128236926+Ridhima-s@users.noreply.github.com>
2023-04-16 22:32:13 +0530

    Update Assignment8.c

--- Assignment8.c
+++ Assignment8.c
@@ -4,5 +4,23 @@
 
 int main()
 {
-  return 0;
+  char name[50],temp;
+	int i,j,count=0;
+	printf("enter a string: ");
+	scanf("%s",&name);	
+	for(i=0;name[i]!='\0';i++)
+	{
+		count++;
+	}
+	for(i=0;i<count-1;i++)
+	{
+		for(j=i+1;j<count;j++)
+		if(name[i]>name[j])
+		{
+			temp=name[i];
+			name[i]=name[j];
+			name[j]=temp;
+		}
+	}
+	printf("%s",name);
 }
